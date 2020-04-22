53f9cf097e18266bb774bcea0d9d8edc2d8bb65e
dabean-games <dabean.games@gmail.com>
2020-04-23 00:05:36 +0200

    1 binaries uploaded

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,50 +1,70 @@
-import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Injectable } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class GameService {
-  gameData={
-    resources:{
-      food:0,
-      wood:10,
-      stone:0,
-      gold:0
+  gameData = {
+    resources: {
+      food: {
+        quantity: 5,
+        url:
+          "https://cdn.jsdelivr.net/gh/dabean-games/town@master/src/assets/images/icons/resources/wd_t_07.png",
+          alt: "Food"
+      },
+      wood: {
+        quantity: 10,
+        url:
+          "https://cdn.jsdelivr.net/gh/dabean-games/town@master/src/assets/images/icons/resources/wd_t_07.png",
+          alt: "Wood"
+      },
+      stone: {
+        quantity: 15,
+        url:
+          "https://cdn.jsdelivr.net/gh/dabean-games/town@master/src/assets/images/icons/resources/wd_t_07.png",
+          alt: "Stone"
+      },
+      gold: {
+        quantity: 1000,
+        url:
+          "https://cdn.jsdelivr.net/gh/dabean-games/town@master/src/assets/images/icons/resources/wd_t_07.png",
+          alt: "Gold"
+      }
     },
-    language:'en'
+    language: "en"
+  };
+  constructor(public translate: TranslateService) {
+    translate.addLangs(["en", "es"]);
+    translate.setDefaultLang("en");
   }
-  constructor(public translate: TranslateService) { 
-    translate.addLangs(['en', 'es']);
-    translate.setDefaultLang('en');
-    }
 
   /* Data */
-  getData(){
+  getData() {
     return this.gameData;
   }
-  setData(data){
-    this.gameData=data;
+  setData(data) {
+    this.gameData = data;
   }
   /* Resources */
-  getResource(resource:string){
+  getResource(resource: string) {
     return this.gameData.resources[resource];
   }
-  setResource(resource:string,quantity:number){
-    this.gameData.resources[resource]=quantity;
+  setResource(resource: string, quantity: number) {
+    this.gameData.resources[resource] = quantity;
   }
 
   /* Language */
-  getLanguages(){
+  getLanguages() {
     return this.translate.getLangs;
   }
 
-  getLanguage(){
+  getLanguage() {
     this.translate.use(this.gameData.language);
     return this.gameData.language;
   }
-  setLanguage(language:string){
-    this.gameData.language=language;
+  setLanguage(language: string) {
+    this.gameData.language = language;
     this.translate.use(language);
   }
 }
